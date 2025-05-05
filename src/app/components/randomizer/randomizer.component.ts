e9b5c1b216d23bfd59cdd1ebcc8b2fc5c0d3616f
LorenzoPuppo95 <puppolorenzo@live.it>
2025-05-05 09:47:59 +0200

    randomize fix

--- src/app/components/randomizer/randomizer.component.ts
+++ src/app/components/randomizer/randomizer.component.ts
@@ -27,8 +27,12 @@ export class RandomizerComponent {
 
   randomizeGroups() {
     this.groups = [];
-    const shuffled = [...this.students].sort(() => 0.5 - Math.random());
-    
+    const shuffled = [...this.students];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+
     while (shuffled.length) {
       this.groups.push(shuffled.splice(0, this.groupSize));
     }
